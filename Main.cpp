fa3da9bfee3244bf28ad13ad58f1e00512a3c98d
Furkan Yigit <bilalyigit594@gmail.com>
2025-02-17 17:25:11 +0300

    init

---
+++ Main.cpp
@@ -0,0 +1,45 @@
+#include <glad/glad.h>
+#include <glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialze GLAD" << std::endl;
+		return -1;
+	}
+
+	int value = glfwWindowShouldClose(window);
+	while (value == GLFW_FALSE) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+		value = glfwWindowShouldClose(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
