22edb5b0c33d5f385e04bf476b4327436adbcd09
Furkan Yigit <bilalyigit594@gmail.com>
2025-03-11 16:54:15 +0300

    [[Exercise 1-2nd try]: So we still use the same VBO for our vertex databut specify to glDrawArrays function that we'd like to draw 6 vertices instead of 3

--- Main.cpp
+++ Main.cpp
@@ -101,18 +101,15 @@ int main() {
 #pragma endregion
 
 #pragma region Set-up vertex buffer and attributes
-	float firstTriangleVertices[] = {
+	float vertices[] = {
 		//firstTriangleVertices
 	   -0.5f, -0.5f, 0.0f, // left  
 		0.5f, -0.5f, 0.0f, // right 
 		0.0f,  0.5f, 0.0f,  // top
-	};
-
-	float secondTriangleVertices[] = {
 		//secondTriangleVertices
 		0.5f, 0.5f, 0.0f, // left  
 		-0.5f, 0.5f, 0.0f, // right 
-		 0.0f,  -0.5f, 0.0f  // top   
+		0.0f,  -0.5f, 0.0f  // top   
 	};
 
 
@@ -124,12 +121,10 @@ int main() {
 	glBindVertexArray(VAO);
 	//then bind and set vertex buffer(s)
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangleVertices), firstTriangleVertices, GL_STATIC_DRAW);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangleVertices), secondTriangleVertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 	//and then configure vertex attributes(s).
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
-	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)sizeof(firstTriangleVertices));
 	glEnableVertexAttribArray(0);
 #pragma endregion
 
@@ -146,7 +141,7 @@ int main() {
 		// draw our first triangle
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 		// glBindVertexArray(0); // no need to unbind it every time 
 
 
