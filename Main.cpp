a44c996da75f18eb51e8f3b38237ae82c5007b6f
Furkan Yigit <bilalyigit594@gmail.com>
2025-03-19 12:05:04 +0300

    [UPDATe]: File structure change for simplicity

--- Main.cpp
+++ Main.cpp
@@ -11,25 +11,6 @@ void process_input(GLFWwindow* window);
 //Settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"//Remember glEnableVertexAttribArray parameter
-"layout (location = 1) in vec3 aColor;\n"//Remember glEnableVertexAttribArray parameter
-"out vec3 ourColor;\n"//Output a color to the fragment shader
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos, 1.0);\n"
-"   ourColor = aColor;\n"
-"}\0";
-
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"in vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(ourColor, 1.0f);\n"
-"}\n\0";
-
 int main() {
 
 #pragma region Init and configure glfw
@@ -59,7 +40,7 @@ int main() {
 #pragma endregion
 
 #pragma region build and compile vertex and fragment shaders
-Shader ourShader("../Shaders/vShader.vs", "../Shaders/fShader.fs");
+Shader ourShader("vShader.vs", "fShader.fs");
 #pragma endregion
 
 #pragma region Set-up vertex buffer and attributes
